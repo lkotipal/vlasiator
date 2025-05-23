e81f86aa904443fd0ab77e0886500bb977b5f1de
Francesco Sgherzi <francesco.sgherzi@sipearl.com>
2025-05-23 12:36:08 +0200

    vlasovsolver/vectorclass_sve: added License header

--- vlasovsolver/vectorclass_sve.h
+++ vlasovsolver/vectorclass_sve.h
@@ -1,7 +1,24 @@
-//
-// Created by francesco.sgherzi on 23/04/25.
-// Based on the code from Roger Gili-Coscojuela
-//
+/*
+ * This file is part of Vlasiator.
+ * Copyright 2025 SiPearl
+ *
+ * For details of usage, see the COPYING file and read the "Rules of the Road"
+ * at http://www.physics.helsinki.fi/vlasiator/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #ifndef VECTORCLASS_SVE_FIXED_H
 #define VECTORCLASS_SVE_FIXED_H
